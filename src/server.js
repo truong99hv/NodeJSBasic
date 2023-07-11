7e89addeef851cf921ef27dbf87683b4c02149e0
HVT <congatruong99pro@gmail.com>
2023-07-11 20:33:13 +0700

    static file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 configViewEngine(app);
 app.get("/", (req, res) => {
